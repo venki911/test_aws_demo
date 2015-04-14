de7e12d55835a09715a445aecf38c1e90a0e8181
Le Anh Thang <le.thang@mulodo.com>
2015-04-14 16:57:09 +0700

    Finish codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root 'uploads#new'
   get 'uploads/new'
 
   get 'uploads/create'
 
   get 'uploads/index'
+
+  resources :uploads
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
